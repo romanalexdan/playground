82beb47691cf942a7a20da4352c8806763bf54e5
Alex R. <alexz.danr@gmail.com>
2026-03-21 16:08:45 +0200

    Add a new test

--- src/test/java/SimpleTestNGTest.java
+++ src/test/java/SimpleTestNGTest.java
@@ -3,6 +3,8 @@ import org.testng.annotations.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.math.BigDecimal;
+
 public class SimpleTestNGTest {
 
     private static final Logger logger = LoggerFactory.getLogger(SimpleTestNGTest.class);
@@ -34,12 +36,27 @@ public class SimpleTestNGTest {
     @Test
     public void test() {
         logger.info("In test 1");
-        Assert.assertEquals(4 + 4, 8, "Test sum is 8");
+        int a = 4;
+        int b = 5;
+        int result = 9;
+        Assert.assertEquals(a + b, result, "Test sum is "+ result);
     }
 
     @Test
     public void test2() {
         logger.info("In test 2");
-        Assert.assertEquals(5+5, 10, "test 2");
+        int a = 5;
+        int b = 2;
+        int result = 3;
+        Assert.assertEquals(a - b, result, "Test difference is "+ result);
+    }
+
+    @Test
+    public void test3() {
+        logger.info("In test 3");
+        BigDecimal a = new BigDecimal("10.02");
+        BigDecimal b = new BigDecimal("10.020001");
+
+        Assert.assertEquals(a.compareTo(b), -1, "Test values should be different.");
     }
 }

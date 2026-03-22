8843ec36fd1eabc8ec9672b3d2f97b0313e25f68
Alex R. <alexz.danr@gmail.com>
2026-03-22 15:19:49 +0200

    Refactor models for standard API convention

--- src/main/java/org/example/models/KycStatus.java
+++ src/main/java/org/example/models/KycStatus.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
-public enum OrderStatus {
+public enum KycStatus {
     APPROVED("APPROVED"),
     PENDING("PENDING"),
     FAILED("FAILED");
